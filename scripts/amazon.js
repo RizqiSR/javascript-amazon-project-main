5ebde35fe3c33b40e5dcc864b61f977f425cf06a
RizqiSR <rizqi.srahmah27@gmail.com>
2023-09-13 14:28:18 +0700

    Use product.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,34 +1,3 @@
-const products = [
-  {
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87,
-    },
-    // JS has problems doing math with floats
-    // Best practice when calculating money, is calculate in cents instead of dollars: $1 = 100 cents
-    priceCents: 1090,
-  },
-  {
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-      stars: 4,
-      count: 127,
-    },
-    priceCents: 2095,
-  },
-  {
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Intermediate Size BasketbalAdults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      stars: 4.5,
-      count: 56,
-    },
-    priceCents: 799,
-  },
-];
 
 let productsHTML = "";
 
